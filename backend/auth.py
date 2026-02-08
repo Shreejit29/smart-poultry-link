6c658c4c3868b7dbde6a1ad6e81377594f3b9c68
Shreejit29 <udekarp007@gmail.com>
2026-02-08 11:32:43 +0530

    Update auth.py

--- backend/auth.py
+++ backend/auth.py
@@ -11,10 +11,12 @@ ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
 
+
 def hash_password(password: str) -> str:
-    return pwd_context.hash(password)
-
-
+    # bcrypt supports max 72 bytes
+    safe_password = password.encode("utf-8")[:72].decode("utf-8", errors="ignore")
+    return pwd_context.hash(safe_password)
+    
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
