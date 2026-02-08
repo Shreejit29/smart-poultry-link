142b99d57643ec202a909159ef63b9a8c5568b33
Shreejit29 <udekarp007@gmail.com>
2026-02-08 12:35:00 +0530

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -2,6 +2,7 @@ import streamlit as st
 from login import login_ui
 from consumer import consumer_ui
 from admin import admin_ui
+from farmer import farmer_ui
 
 st.set_page_config(page_title="Smart Poultry Link", layout="centered")
 
@@ -10,12 +11,12 @@ st.title("🐔 Smart Poultry Link")
 if "token" not in st.session_state:
     login_ui()
 else:
-    st.success("Logged in")
-
     role = st.session_state.get("role")
 
     if role == "consumer":
         consumer_ui()
+    elif role == "farmer":
+        farmer_ui()
     elif role == "admin":
         admin_ui()
     else:
